956f60dab031377140523b065793dde96f33a133
Gerald Amezcua <gamezcua1@hotmail.com>
2018-04-07 23:13:51 -0500

    Comments for projects

---
+++ src/components/Projects/ProjectComments.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import {Comment, Header, Form, Button } from 'semantic-ui-react'
+import {connect} from 'react-redux'
+import {database} from '../../firebase'
+import moment from 'moment';
+import _ from 'lodash';
+
+class PComments extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      comments:[],
+
+      newComment:null
+    };
+
+    this.submitComment = this.submitComment.bind(this);
+  }
+
+  componentDidMount() {
+    database.ref('COMMENTS/'+this.props.users.projectSeen.ID).on('value',(snapshot)=>{
+      this.setState({comments:snapshot.val()})
+    })
+  }
+  
+  submitComment(){
+    let comment = {
+      autor: this.props.users.userLogged.fullName,
+      photo: this.props.users.userLogged.photo,
+      text: this.state.newComment,
+      date: moment().format('L')
+    }
+    
+    database.ref('COMMENTS/'+this.props.users.projectSeen.ID).push(comment);
+  }
+
+  render() {
+    return (
+      <div>
+        <Comment.Group>
+          <Header as='h3' dividing>Comments</Header>
+          {
+            _.map(this.state.comments, (c, i) => {
+              return(
+                <Comment>
+                  <Comment.Avatar src={c.photo}/>
+                  <Comment.Content>
+                    <Comment.Author as='a'>{c.autor}</Comment.Author>
+                    <Comment.Metadata>
+                      <div>{'      '}{c.date}</div>
+                    </Comment.Metadata>
+                    <Comment.Text>{c.text}</Comment.Text>
+                  </Comment.Content>
+                </Comment >
+              )
+            })  
+          }
+          <Form reply>
+            <Form.TextArea onChange={(e)=>{this.setState({newComment:e.target.value})}}/>
+            <Button onClick={this.submitComment} content='Add Comment' labelPosition='left' icon='edit' primary />
+          </Form>
+        </Comment.Group>
+      </div>
+    );
+  }
+}
+
+let ProjectComments = (PComments);
+ProjectComments = connect(state => ({ users: state.users }), {})(ProjectComments);
+export default ProjectComments;
--- src/components/Projects/ProjectDescription.jsx
+++ src/components/Projects/ProjectDescription.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {Container, Image, Header, Button, Tab, Divider} from 'semantic-ui-react'
+import ProjectComments from './ProjectComments'
+//.projectSeen
 
+class PDescription extends React.Component {  
 
-class ProjectDescription extends React.Component {
-  render () {
+  renderProjectInfo(){
     return(
       <div>
-        <h1>Project</h1>
+      <Image src={this.props.users.projectSeen.photo} size='small' verticalAlign='middle' />
+      <Container>
+        <Header as='h1'>{this.props.users.projectSeen.name}</Header>
+        <p>{this.props.users.projectSeen.shortDescription}</p>
+      </Container>
+      </div>
+    );
+  }
+
+  render () {
+    const panes = [
+      { menuItem: 'Project Information', render: () => <Tab.Pane>{this.renderProjectInfo()}</Tab.Pane> },
+      { menuItem: 'Project PDF', render: () => <Tab.Pane>Project PDF</Tab.Pane> },
+      { menuItem: 'Discussion', render: ()=> <Tab.Pane><ProjectComments/></Tab.Pane>}
+    ]
+
+    return(
+      <div style={{ margin: "1.5%" }}>
+      <Header as='h1'>{this.props.users.projectSeen.name}</Header>
+      <Divider/>
+        <Tab panes={panes} /> 
       </div>
     );
   }
 }
 
+let ProjectDescription = (PDescription);
+ProjectDescription = connect(state => ({ users: state.users }), {  })(ProjectDescription);
 export default ProjectDescription;
